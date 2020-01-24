5204f1f5c85e101db40aa45754d6c88735508166
Kemal DAG <gkemaldag@gmail.com>
2020-01-24 23:07:08 +0300

    Update TODO

--- backend/app.js
+++ backend/app.js
@@ -11,11 +11,12 @@ const loginMiddleware = require(path.resolve('middleware', 'login'))
 const authenticationMiddleware = require(path.resolve('middleware', 'authentication'))
 const apiMiddleware = require(path.resolve('middleware', 'api'))
 
-app.use(basicAuth({
-  users: { 'admin': '243243' },
-  challenge: true,
-  realm: 'FormPress Beta'
-}))
+// app.use(basicAuth({
+//   users: { 'admin': '243243' },
+//   challenge: true,
+//   realm: 'FormPress Beta'
+// }))
+
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
@@ -39,14 +40,16 @@ apiMiddleware(app)
 
 if (process.env.FP_ENV === 'production') {
   app.use(express.static('/frontend/build'))  
+
+  const staticIndexHtml = fs.readFileSync('/frontend/build/index.html', { encoding: 'utf8' })
+
+  // Send built index.html to allow refreshes to work
+  app.get('*', (req, res) => {
+    res.header('Content-type', 'text/html')
+    res.status(200).send(staticIndexHtml)
+  })
 }
 
-const staticIndexHtml = fs.readFileSync('/frontend/build/index.html', { encoding: 'utf8' })
 
-// Send built index.html to allow refreshes to work
-app.get('*', (req, res) => {
-  res.header('Content-type', 'text/html')
-  res.status(200).send(staticIndexHtml)
-})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
