1981a820d0e0dce7cf6bd33dde43dfae999e172e
Tunar Mahmudov <tunardev14@gmail.com>
2020-07-13 17:28:33 +0400

    Dropdown with Semantic library

--- frontend/src/modules/elements/index.js
+++ frontend/src/modules/elements/index.js
@@ -1,7 +1,8 @@
 import './index.css'
+import 'semantic-ui-css/semantic.min.css'
 
 export {default as Button} from './Button.js'
 export {default as Checkbox} from './Checkbox.js'
 export {default as Text} from './Text.js'
 export {default as TextArea} from './TextArea.js'
-export {default as Dropdown} from './Dropdown.js'
+export {default as CustomDropdown} from './CustomDropdown.js'
