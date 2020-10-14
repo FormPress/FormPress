452e69d647d33d668737cdacec73d56f3551d503
Tunar Mahmudov <tunardev14@gmail.com>
2020-10-14 20:27:26 +0400

    Name field was added

---
+++ frontend/src/modules/elements/Name.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+import EditableLabel from '../common/EditableLabel'
+import ElementContainer from '../common/ElementContainer'
+
+export default class Name extends Component {
+  static weight = 7
+
+  static defaultConfig = {
+    id: 0,
+    type: 'Name',
+    label: 'Name'
+  }
+
+  static configurableSettings = {
+    required: {
+      default: false,
+      formProps: {
+        type: 'Checkbox',
+        label: 'Make this field required?'
+      }
+    }
+  }
+
+  render() {
+    const { config, mode } = this.props
+    const inputProps = {}
+
+    if (typeof config.value !== 'undefined') {
+      inputProps.value = config.value
+    }
+
+    if (typeof this.props.onChange !== 'undefined') {
+      inputProps.onChange = this.props.onChange
+    }
+
+    return (
+      <ElementContainer type={config.type} {...this.props}>
+        <EditableLabel
+          className='fl label'
+          mode={mode}
+          labelKey={config.id}
+          handleLabelChange={this.props.handleLabelChange}
+          value={config.label}
+          required={config.required}
+        />
+        <table>
+          <tr>
+              <td>
+                <label for='firstname'>First name:</label>
+              </td>
+              <td>
+                <input type='text' id='fname' name='firstname'></input>
+              </td>
+          </tr>
+          <tr>
+              <td>
+                <label for='lastname'>Last name:</label>
+              </td>
+              <td>
+                <input type='text' id='lname' name='lastname'></input>
+              </td>
+          </tr>
+        </table>
+      </ElementContainer>
+    )
+  }
+}
