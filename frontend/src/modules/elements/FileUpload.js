00c62055cd222dd1cce12ccf10802dfc9260aaa5
Tunar Mahmudov <tunardev14@gmail.com>
2020-10-20 17:56:50 +0400

    basic file upload

---
+++ frontend/src/modules/elements/FileUpload.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import EditableLabel from '../common/EditableLabel'
+import ElementContainer from '../common/ElementContainer'
+
+import './FileUpload.css'
+
+export default class FileUpload extends Component {
+  static weight = 8
+
+  static defaultConfig = {
+    id: 0,
+    type: 'FileUpload',
+    label: 'File Upload'
+  }
+
+  static configurableSettings = {
+    required: {
+      default: false,
+      formProps: {
+        type: 'Checkbox',
+        label: 'Make this field required?'
+      }
+    }
+  }
+
+  render() {
+    const { config, mode } = this.props
+    const inputProps = {}
+
+    if (typeof config.value !== 'undefined') {
+      inputProps.value = config.value
+    }
+
+    if (typeof this.props.onChange !== 'undefined') {
+      inputProps.onChange = this.props.onChange
+    }
+
+    return (
+      <ElementContainer type={config.type} {...this.props}>
+        <EditableLabel
+          className='fl label'
+          mode={mode}
+          labelKey={config.id}
+          handleLabelChange={this.props.handleLabelChange}
+          value={config.label}
+          required={config.required}
+        />
+        <form class="file-upload-form-class" id="file-upload-form-id">
+          <input type="file" id="file-upload" name="fileUpload" />
+          
+        </form>
+        
+      </ElementContainer>
+    )
+  }
+}
