97f0c5fdea958506708f4a4b889e8798fc14903b
Kemal DAG <gkemaldag@gmail.com>
2021-02-26 11:51:10 +0300

    Add instructions to embed

--- frontend/src/runtime/embed.js
+++ frontend/src/runtime/embed.js
@@ -1,4 +1,26 @@
+/*
+  Embed should work by this
+  <div>
+    <script src="http://localhost:3001/runtime/embed.js" FP_ID="7"></script>
+  </div>
+
+  => 
+
+  <div>
+    <script src="http://localhost:3001/runtime/embed.js" FP_ID="7"></script>
+    <iframe src="http://localhost:3001/form/view/7"/>
+  </div>
+  
+  1. DON't depend on BACKEND, parse it from src part of script tags who has FP_ID attribute defined
+
+  2. traverse FP_ID defined script tags, and insert iframe form view just as sibling
+
+
+*/
+
 ;(async () => {
+  let BACKEND = false
+
   const loadScript = (name) =>
     new Promise((resolve, reject) => {
       const script = document.createElement('script')
@@ -8,7 +30,9 @@
       document.head.appendChild(script)
     })
 
-  await loadScript(`${BACKEND}/runtime/3rdparty/`)
+  // fill BACKEND variable,
 
-  console.log(jQuery('script'))
+  await loadScript('3rdparty/jquery.3.4.1.min')
+
+  console.log(jQuery('script a'))
 })()
