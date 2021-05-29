41e1e868ab627bac7eb0253dcc31109b9d99329a
Cem Özder <cm.ozder@gmail.com>
2021-05-29 13:20:54 +0300

    Pipeline test for backend 10

--- backend/helper/oldformpropshandler.js
+++ backend/helper/oldformpropshandler.js
@@ -1,18 +1,18 @@
-const path = require('path')
+/*const path = require('path')
 
 const { getConfigurableSettings } = require(path.resolve(
   './',
   'script',
   'transformed',
   'ConfigurableSettings.js'
-))
+))*/
 
 const { cloneDeep } = require('lodash')
 
 exports.updateFormPropsWithNewlyAddedProps = (formProps) => {
   let updatedFormProps = cloneDeep(formProps)
 
-  for (const formElement of updatedFormProps.elements) {
+  /* for (const formElement of updatedFormProps.elements) {
     for (const elem in getConfigurableSettings(formElement.type)) {
       if (elem in formElement !== true) {
         formElement[elem.toString()] = getConfigurableSettings(
@@ -20,7 +20,7 @@ exports.updateFormPropsWithNewlyAddedProps = (formProps) => {
         )[elem].default
       }
     }
-  }
+  }*/
 
   return updatedFormProps
 }
