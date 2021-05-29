c334c5167828290b271729bcefd2a63c89ea79b9
Cem Özder <cm.ozder@gmail.com>
2021-05-29 12:55:33 +0300

    Pipeline test for backend 9

--- backend/helper/oldformpropshandler.js
+++ backend/helper/oldformpropshandler.js
@@ -1,8 +1,30 @@
 const path = require('path')
+const fs = require('fs')
+
+const sourcePath = path.resolve(
+  './',
+  'script',
+  'transformed',
+  'ConfigurableSettings.js'
+)
+
+console.log(sourcePath)
+
+fs.access(sourcePath, fs.F_OK, (err) => {
+  if (err) {
+    console.log('File is not exist and we have a problem.')
+    console.error(err)
+    return
+  } else {
+    console.log('File is already exist. But we have another problem.')
+  }
+})
 
 const { getConfigurableSettings } = require(path.resolve(
-  'script/transformed',
-  'ConfigurableSettings'
+  './',
+  'script',
+  'transformed',
+  'ConfigurableSettings.js'
 ))
 
 const { cloneDeep } = require('lodash')
