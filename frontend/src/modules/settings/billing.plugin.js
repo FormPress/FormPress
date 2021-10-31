98fdbb30625ed69346101ac970e1bc60a874af88
Kemal DAG <gkemaldag@gmail.com>
2021-10-31 22:25:25 +0300

    Production release command added

---
+++ frontend/src/modules/settings/billing.plugin.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export default class Billing extends Component {
+  static path = '/settings/billing'
+  static menuText = 'Billing'
+
+  render() {
+    return 'Billing Content'
+  }
+}
