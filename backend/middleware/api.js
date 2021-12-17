e6840a9a3832fa57f2a131caf43281ed68e77005
Omer Faruk KORKMAZ <mrfrkkorkmaz@gmail.com>
2021-12-17 14:50:02 +0300

    workaround for tests, tests should be updated

--- backend/middleware/api.js
+++ backend/middleware/api.js
@@ -105,7 +105,7 @@ module.exports = (app) => {
     async (req, res) => {
       const { user_id } = req.params
 
-      if (res.locals.auth.permission.admiin) {
+      if (res.locals.auth.permission.admin) {
         res.status(200).json({ message: 'new' })
       } else {
         const db = await getPool()
