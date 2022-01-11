cdd23515941af8f017b1959a811460b676dc35c4
Cem Özder <cm.ozder@gmail.com>
2022-01-11 07:51:11 +0300

    small migration changes.

--- backend/migration/migration-20220104-007.js
+++ backend/migration/migration-20220104-007.js
@@ -4,8 +4,10 @@ module.exports = async (db) => {
       \`id\` int(11) unsigned NOT NULL AUTO_INCREMENT,
       \`user_id\` int(11) DEFAULT NULL,
       \`createdDate\` datetime DEFAULT NULL,
-      \`paymentRequest\` longtext,
-      \`paymentResponse\` longtext,
+      \`request\` longtext,
+      \`response\` longtext,
+      \`status\` tinytext,
+      \`type\` tinytext,
       PRIMARY KEY (\`id\`),
       KEY \`user_id\` (\`user_id\`)
     ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
