80ddf10a483dcb04a4ccd16c7f9249bd8472fe11
Cem Özder <cm.ozder@gmail.com>
2022-03-01 13:48:24 +0300

    some css and file upload bugs fixed.

--- frontend/src/modules/elements/FileUpload.js
+++ frontend/src/modules/elements/FileUpload.js
@@ -66,7 +66,7 @@ export default class FileUpload extends Component {
           className="fl label"
           mode={mode}
           labelKey={config.id}
-          data-placeholder="Type a question"
+          dataPlaceholder="Type a question"
           handleLabelChange={this.props.handleLabelChange}
           value={config.label}
           required={config.required}
@@ -75,7 +75,7 @@ export default class FileUpload extends Component {
         <div className="clearfix">
           <EditableLabel
             className="sublabel"
-            data-placeholder="Type a sublabel"
+            dataPlaceholder="Click to edit sublabel"
             mode={mode}
             labelKey={`sub_${config.id}`}
             handleLabelChange={this.props.handleLabelChange}
