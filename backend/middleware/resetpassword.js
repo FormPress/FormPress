19c2a780849a48f5a35cb036f812e6060fb4a7f7
Cem Özder <cm.ozder@gmail.com>
2022-03-30 05:11:31 +0300

    prettier issue resolved.

--- backend/middleware/resetpassword.js
+++ backend/middleware/resetpassword.js
@@ -25,7 +25,7 @@ module.exports = (app) => {
             res.status(403).json({
               message: 'New password must contain at least 8 characters.'
             })
-          }else{
+          } else {
             const expireDate = new Date(
               Date.parse(result[0].passwordResetCodeExpireAt)
             )
