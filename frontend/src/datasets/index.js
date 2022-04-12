beb0cfe69cd944d95ff3bcfaae99db30b9812e2a
Oguzhan EREN <oguzhan.ern@hotmail.com>
2022-04-12 15:42:01 +0300

    wording

--- frontend/src/datasets/index.js
+++ frontend/src/datasets/index.js
@@ -1,11 +1,11 @@
 const datasets = [
-  { value: 'world', displayText: 'World', disabled: true },
+  { value: 'world', displayText: '- World -', disabled: true },
   { value: 'countries', displayText: 'Countries' },
   { value: 'calendar', displayText: '- Calendar & Date -', disabled: true },
   { value: 'months', displayText: 'Months' },
+  { value: 'years', displayText: 'Years' },
   { value: 'daysOfWeek', displayText: 'Days of Week' },
-  { value: 'daysOfMonth', displayText: 'Days of Month' },
-  { value: 'years', displayText: 'Years' }
+  { value: 'daysOfMonth', displayText: 'Days of Month' }
 ]
 
 module.exports = datasets
