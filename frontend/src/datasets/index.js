c3a1ff0d75921f4635267a8069da1a3708d08e18
Oguzhan EREN <oguzhan.ern@hotmail.com>
2022-04-12 15:26:39 +0300

    added new datasets

--- frontend/src/datasets/index.js
+++ frontend/src/datasets/index.js
@@ -1,3 +1,11 @@
-const datasets = ['countries', 'months']
+const datasets = [
+  { value: 'world', displayText: 'World', disabled: true },
+  { value: 'countries', displayText: 'Countries' },
+  { value: 'calendar', displayText: '- Calendar & Date -', disabled: true },
+  { value: 'months', displayText: 'Months' },
+  { value: 'daysOfWeek', displayText: 'Days of Week' },
+  { value: 'daysOfMonth', displayText: 'Days of Month' },
+  { value: 'years', displayText: 'Years' }
+]
 
 module.exports = datasets
