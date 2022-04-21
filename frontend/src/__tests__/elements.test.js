5939cff801a095b9f01cb3a9e7f26bba2be8d363
Oguzhan EREN <oguzhan.ern@hotmail.com>
2022-04-21 17:10:11 +0300

    element test address excluded

--- frontend/src/__tests__/elements.test.js
+++ frontend/src/__tests__/elements.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { create } from 'react-test-renderer'
 import * as Elements from '../modules/elements'
 
-const excludedKeys = ["Button", "Header", "Separator"]
+const excludedKeys = ['Button', 'Header', 'Separator', 'Address']
 const keys = Object.keys(Elements)
 for (const key of keys) {
   const Component = Elements[key]
@@ -14,21 +14,30 @@ for (const key of keys) {
     })
 
     test('Can be rendered without errors', () => {
-      const component = create(<Component config={ Component.defaultConfig }/>)
+      const component = create(<Component config={Component.defaultConfig} />)
     })
 
-    test('Has a valid <name> attribute', () => { //what is valid? "q_{id}"
-      if (!excludedKeys.includes(key)) { //these elements dont have name attribute
-        const component = create(<Component config={ Component.defaultConfig } mode={ "not build" }/>)
-        if (key === "Name") {//Name has 2 values; first name and last name. Test should reconsidered for multiple value fields
+    test('Has a valid <name> attribute', () => {
+      //what is valid? "q_{id}"
+      if (!excludedKeys.includes(key)) {
+        //these elements dont have name attribute
+        const component = create(
+          <Component config={Component.defaultConfig} mode={'not build'} />
+        )
+        if (key === 'Name') {
+          //Name has 2 values; first name and last name. Test should reconsidered for multiple value fields
           const myTestObject = component.root
-          const nameAttributes = myTestObject.findAllByProps({name:"q_0[firstName]"})
-          const multipleNames = nameAttributes.concat(myTestObject.findAllByProps({name:"q_0[lastName]"}))
+          const nameAttributes = myTestObject.findAllByProps({
+            name: 'q_0[firstName]'
+          })
+          const multipleNames = nameAttributes.concat(
+            myTestObject.findAllByProps({ name: 'q_0[lastName]' })
+          )
           expect(multipleNames.length).toBeGreaterThan(1)
         } else {
           const myTestObject = component.root
           //assuming defaultConfig have id:0
-          const nameAttributes = myTestObject.findAllByProps({name:"q_0"})//if its static "q_0" it still passes. should be improved
+          const nameAttributes = myTestObject.findAllByProps({ name: 'q_0' }) //if its static "q_0" it still passes. should be improved
           const nameCount = nameAttributes.length
           expect(nameCount).toBeGreaterThan(0)
         }
