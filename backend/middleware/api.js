0f02796e0326fd275e6e2d77a38963de3d10a103
Cem Özder <cm.ozder@gmail.com>
2022-04-28 14:30:05 +0300

    small sql bug fixed.

--- backend/middleware/api.js
+++ backend/middleware/api.js
@@ -87,7 +87,7 @@ module.exports = (app) => {
       const db = await getPool()
       const user_id = req.params.user_id
       const formResults = await db.query(
-        `SELECT f.*, CASE WHEN (SELECT id FROM form_published WHERE form_id = f.id) IS NULL THEN 0 ELSE id END AS published_id, ( SELECT COUNT(*) FROM submission WHERE form_id = f.id ) as responseCount FROM form AS f WHERE f.user_id = ? AND deleted_at IS NULL`,
+        `SELECT f.*, CASE WHEN (SELECT id FROM form_published WHERE form_id = f.id) IS NULL THEN 0 ELSE id END AS published_id, ( SELECT COUNT(*) FROM submission WHERE form_id = f.id ) as responseCount, ( SELECT COUNT(*) FROM submission WHERE form_id = \`form\`.\`id\` AND \`read\` = 0 ) as unreadCount FROM form AS f WHERE f.user_id = ? AND deleted_at IS NULL`,
         [user_id]
       )
 
