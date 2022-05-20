cab22cddd190be0b1f3c4e707614aa2c63cb4877
Cem Özder <cm.ozder@gmail.com>
2022-05-20 18:47:16 +0300

    404 page is ready to test.

--- frontend/src/modules/helper/Settings.js
+++ frontend/src/modules/helper/Settings.js
@@ -1,56 +1,81 @@
 import React, { Component } from 'react'
-import { NavLink, Switch, Route } from 'react-router-dom'
+import { NavLink, Switch, Route, Redirect } from 'react-router-dom'
+import PrivateRoute from '../../PrivateRoute'
 import './Settings.css'
 import { api } from '../../helper'
 
 class Settings extends Component {
   state = {
-    settingComponents: []
+    navLinks: [],
+    routes: [],
+    planNames: ['silver', 'gold', 'diamond']
   }
   async componentDidMount() {
     const { data } = await api({
       resource: `/api/app/get/settingsPluginfileslist`
     })
 
-    data.forEach((file) => {
-      import('../settings/' + file).then((module) => {
-        const newSettingComponents = [
-          ...this.state.settingComponents,
-          {
-            path: module.default.path,
-            text: module.default.menuText,
-            component: module.default
-          }
-        ]
+    const navLinks = [],
+      routes = [
+        <Route exact path="/settings" key="changepassword">
+          <Redirect to="/settings/changepassword" />
+        </Route>
+      ]
 
-        this.setState({
-          settingComponents: newSettingComponents
-        })
+    for (const file of data) {
+      await import('../settings/' + file).then((module) => {
+        navLinks.push(
+          <NavLink
+            key={module.default.menuText}
+            exact
+            to={module.default.path}
+            activeClassName="selected">
+            {module.default.menuText}
+          </NavLink>
+        )
+        routes.push(
+          <PrivateRoute
+            exact
+            path={module.default.path}
+            component={module.default}
+            key={module.default.menuText}
+          />
+        )
+        if (module.default.menuText === 'Billing') {
+          this.state.planNames.map((item, key) => {
+            routes.push(
+              <PrivateRoute
+                exact
+                path={`/settings/billing/${item}`}
+                component={module.default}
+                key={item}
+              />
+            )
+          })
+        }
       })
+    }
+
+    routes.push(<Redirect path="*" to="/404" key={routes.length + 1} />)
+
+    this.setState({
+      navLinks,
+      routes
     })
   }
+
   render() {
+    const { navLinks, routes } = this.state
+
     return (
       <div className="settings_container">
         <div className="content">
           <div className="settings_menu">
             <h2>User settings</h2>
-            {this.state.settingComponents.map((item, key) => (
-              <NavLink
-                key={key}
-                exact
-                to={item.path}
-                activeClassName="selected">
-                {item.text}
-              </NavLink>
-            ))}
+            {navLinks}
           </div>
           <div className="settings_content">
-            <Switch>
-              {this.state.settingComponents.map((item, key) => (
-                <Route path={item.path} component={item.component} key={key} />
-              ))}
-            </Switch>
+            <Switch>{routes}</Switch>
           </div>
         </div>
       </div>
