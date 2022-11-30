6766dbccf867521f4185a5fd6d8a30d6fc843471
Omer Faruk KORKMAZ <mrfrkkorkmaz@gmail.com>
2022-11-30 13:06:09 +0300

    221 rename number element

--- frontend/src/modules/internal/index.js
+++ frontend/src/modules/internal/index.js
@@ -1,3 +1,3 @@
 export { default as Password } from './Password.js'
-export { default as Number } from './Number.js'
+export { default as NumberE } from './NumberE.js'
 export { default as List } from './List.js'
