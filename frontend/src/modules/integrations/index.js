c6b37d5b10442b311910a098a2ab23c3543d8f95
cemalfratdag <cemalfratdag@gmail.com>
2023-02-10 01:46:18 +0300

    add slack webhook integration prototype ui

--- frontend/src/modules/integrations/index.js
+++ frontend/src/modules/integrations/index.js
@@ -1,3 +1,4 @@
 export { default as GoogleDrive } from './GoogleDrive.js'
 export { default as GoogleSheets } from './GoogleSheets.js'
 export { default as Discord } from './Discord.js'
+export { default as Slack } from './Slack.js'
