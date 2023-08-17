c93eb76f9258ef00b8ecc3618f2e71ed6f031f5e
Oguzhan Eren <oguzhan.ern@hotmail.com>
2023-08-17 13:58:05 +0300

    added location elem

---
+++ frontend/src/modules/elements/Location.js
@@ -0,0 +1,296 @@
+import React, { Component } from 'react'
+
+import EditableLabel from '../common/EditableLabel'
+import ElementContainer from '../common/ElementContainer'
+import { faMapLocation, faWarning } from '@fortawesome/free-solid-svg-icons'
+
+import './Location.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+export default class Location extends Component {
+  constructor(props) {
+    super(props)
+
+    this.promptOnError = this.promptOnError.bind(this)
+    this.promptOnSuccess = this.promptOnSuccess.bind(this)
+
+    this.state = {
+      errorMessage: '',
+      inputValues: {
+        latitude: '',
+        longitude: ''
+      }
+    }
+  }
+
+  static weight = 14
+
+  static defaultConfig = {
+    id: 0,
+    type: 'Location',
+    label: 'Location'
+  }
+
+  static metaData = {
+    icon: faMapLocation,
+    displayText: 'Location',
+    group: 'inputElement'
+  }
+
+  static configurableSettings = {
+    hideManualInputs: {
+      default: false,
+      formProps: {
+        type: 'Checkbox',
+        label: '',
+        options: ['Hide coordinate input fields']
+      }
+    }
+  }
+
+  static submissionHandler = {
+    findQuestionValue: (inputs, qid) => {
+      console.log(inputs, qid)
+      let valueObject = {}
+      for (const elem of inputs) {
+        if (elem.q_id === qid) {
+          valueObject[elem.sub_id] = elem.value
+        }
+      }
+      return valueObject
+    }
+  }
+
+  static helpers = {
+    getElementValue: (id) => {
+      const locationElem = document.getElementById(`qc_${id}`)
+      const locationInputs = locationElem.querySelectorAll('input[type="text"]')
+
+      const values = []
+
+      for (const input of locationInputs) {
+        values.push({
+          type: input.dataset.coordinatetype,
+          value: input.value
+        })
+      }
+
+      return values
+    },
+    isFilled: (values) => {
+      let filled = 0
+      for (const value of values) {
+        if (value.value !== '') {
+          filled++
+        }
+      }
+      return filled >= 2
+    }
+  }
+
+  static getPlainStringValue(entry) {
+    let plainString
+    if (entry.value !== '') {
+      plainString = Object.entries(entry.value)
+        .map(([, t]) => `${t}`)
+        .join(' ')
+    } else {
+      plainString = '-'
+    }
+    return plainString
+  }
+
+  static renderDataValue(entry, question) {
+    const htmlCollection = []
+    Object.entries(entry.value).map((entry) => {
+      const key = entry[0]
+      const value = entry[1]
+
+      let defaultSublabel = true
+
+      if (question[`${key}SublabelText`]) {
+        defaultSublabel = false
+      }
+
+      htmlCollection.push(
+        <div key={key}>
+          <strong
+            style={defaultSublabel ? { textTransform: 'capitalize' } : null}>
+            {defaultSublabel ? key : question[`${key}SublabelText`]}:
+          </strong>
+          &nbsp; {value}
+          <br />
+        </div>
+      )
+    })
+
+    // means both latitude and longitude are present
+    if (htmlCollection.length === 2) {
+      htmlCollection.push(
+        <div key="map">
+          <strong>Map:</strong>
+          &nbsp;
+          <a
+            href={`https://www.google.com/maps/search/?api=1&query=${entry.value.latitude},${entry.value.longitude}`}
+            target="_blank"
+            rel="noopener noreferrer">
+            Open in Google Maps
+          </a>
+        </div>
+      )
+    }
+
+    return htmlCollection || '-'
+  }
+
+  promptOnSuccess(position) {
+    // update state
+    this.setState({
+      errorMessage: '',
+      inputValues: {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      }
+    })
+  }
+
+  promptOnError(error) {
+    let { errorMessage } = this.state
+
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+        errorMessage = 'User denied the request for Geolocation.'
+        break
+      case error.POSITION_UNAVAILABLE:
+        errorMessage = 'Location information is unavailable.'
+        break
+      case error.TIMEOUT:
+        errorMessage = 'The request to get user location timed out.'
+        break
+      case error.UNKNOWN_ERROR:
+        errorMessage = 'An unknown error occurred.'
+        break
+    }
+
+    this.setState({ errorMessage })
+  }
+
+  render() {
+    const { config, mode } = this.props
+    const { inputValues, errorMessage } = this.state
+
+    const errorPresent = errorMessage !== ''
+
+    return (
+      <ElementContainer type={config.type} {...this.props}>
+        <>
+          <div className="elemLabelTitle">
+            <EditableLabel
+              className="fl label"
+              mode={mode}
+              labelKey={config.id}
+              handleLabelChange={this.props.handleLabelChange}
+              dataPlaceholder="Type a question"
+              value={config.label}
+              required={config.required}
+            />
+          </div>
+          <div className="input">
+            <button
+              type="button"
+              className="latlong-button"
+              onClick={() => {
+                if (navigator.geolocation) {
+                  navigator.geolocation.getCurrentPosition(
+                    // success callback
+                    (position) => {
+                      this.promptOnSuccess(position, inputValues)
+                    },
+                    // error callback
+                    (error) => {
+                      this.promptOnError(error, inputValues)
+                    }
+                  )
+                } else {
+                  this.setState({
+                    errorMessage:
+                      'Geolocation is not supported by this browser.'
+                  })
+                }
+              }}>
+              Fill In Current Location
+            </button>
+            <div
+              className={
+                'latlong-fields' + (config.hideManualInputs ? ' dn' : '')
+              }>
+              <div>
+                <EditableLabel
+                  className="sublabel latlong-label"
+                  dataPlaceholder="Click to edit sublabel"
+                  mode={mode}
+                  labelKey={`location_${config.id}_latitude`}
+                  handleLabelChange={this.props.handleLabelChange}
+                  value={
+                    typeof config.latitudeSublabelText !== 'undefined'
+                      ? config.latitudeSublabelText
+                      : 'Latitude'
+                  }
+                />
+                <input
+                  type="text"
+                  className="latlong-input"
+                  data-coordinateType="latitude"
+                  name={`q_${config.id}[latitude]`}
+                  readOnly={mode === 'builder'}
+                  value={inputValues.latitude}
+                />
+              </div>
+              <div>
+                <EditableLabel
+                  className="sublabel latlong-label"
+                  dataPlaceholder="Click to edit sublabel"
+                  mode={mode}
+                  labelKey={`location_${config.id}_longitude`}
+                  handleLabelChange={this.props.handleLabelChange}
+                  value={
+                    typeof config.longitudeSublabelText !== 'undefined'
+                      ? config.longitudeSublabelText
+                      : 'Longitude'
+                  }
+                />
+                <input
+                  type="text"
+                  className="latlong-input"
+                  data-coordinateType="longitude"
+                  name={`q_${config.id}[longitude]`}
+                  readOnly={mode === 'builder'}
+                  value={inputValues.longitude}
+                />
+              </div>
+            </div>
+          </div>
+          <div className={'response-status' + (errorPresent ? '' : ' dn')}>
+            <FontAwesomeIcon icon={faWarning} className="warning-icon" />
+            &nbsp;{errorMessage}
+          </div>
+          <div className="clearfix">
+            <EditableLabel
+              className="sublabel location-sublabel"
+              dataPlaceholder="Click to edit sublabel"
+              mode={mode}
+              labelKey={`sub_${config.id}`}
+              handleLabelChange={this.props.handleLabelChange}
+              value={
+                typeof config.sublabelText !== 'undefined' &&
+                config.sublabelText !== ''
+                  ? config.sublabelText
+                  : ''
+              }
+            />
+          </div>
+        </>
+      </ElementContainer>
+    )
+  }
+}
---
+++ frontend/src/runtime/location.js
@@ -0,0 +1,74 @@
+;(async () => {
+  function promptOnSuccess(position, questionContainer) {
+    const latInput = questionContainer.querySelector(
+      `input[data-coordinatetype="latitude"]`
+    )
+    const longInput = questionContainer.querySelector(
+      `input[data-coordinatetype="longitude"]`
+    )
+
+    latInput.value = position.coords.latitude
+    longInput.value = position.coords.longitude
+
+    const responseDiv = questionContainer.querySelector(`.response-status`)
+    responseDiv.classList.add(`success`)
+    responseDiv.innerHTML = `Location successfully filled in.`
+    responseDiv.classList.remove(`dn`)
+  }
+
+  function promptOnError(error, questionContainer) {
+    let errorMessage = ''
+
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+        errorMessage = 'User denied the request for Geolocation.'
+        break
+      case error.POSITION_UNAVAILABLE:
+        errorMessage = 'Location information is unavailable.'
+        break
+      case error.TIMEOUT:
+        errorMessage = 'The request to get user location timed out.'
+        break
+      case error.UNKNOWN_ERROR:
+        errorMessage = 'An unknown error occurred.'
+        break
+    }
+
+    const responseDiv = questionContainer.querySelector(`.response-status`)
+    responseDiv.classList.add(`error`)
+    responseDiv.innerHTML = errorMessage
+    responseDiv.classList.remove(`dn`)
+  }
+
+  function onLocationButtonClick(event) {
+    const button = event.target
+    const locationElement = button.closest(`.elementLocation`)
+    const responseDiv = locationElement.querySelector(`.response-status`)
+    // remove all classes from responseDiv other than .response-status and .dn
+    responseDiv.className = `response-status dn`
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        // success callback
+        (position) => {
+          promptOnSuccess(position, locationElement)
+        },
+        // error callback
+        (error) => {
+          promptOnError(error, locationElement)
+        }
+      )
+    } else {
+      responseDiv.classList.add(`error`)
+      responseDiv.innerHTML = `Geolocation is not supported by this browser.`
+      responseDiv.classList.remove(`dn`)
+    }
+  }
+
+  const locationElements = document.querySelectorAll(`.elementLocation`)
+
+  for (const locationElement of locationElements) {
+    const button = locationElement.querySelector(`button`)
+    button.addEventListener(`click`, onLocationButtonClick)
+  }
+})()
